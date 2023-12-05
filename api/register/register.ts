9dfa8087a4ff2b659ca95cbc71668282d4ed88d7
Mohammed Hamadi <114817433+MohammedHamadi@users.noreply.github.com>
2023-12-05 06:14:20 +0100

    Update register.ts

--- api/register/register.ts
+++ api/register/register.ts
@@ -10,33 +10,5 @@ Additional_info) VALUES (${firstname}, ${lastname}, ${email}, ${additional});`;
   } catch (error) {
     return res.json({ error }, { status: 500 });
   }
-  return res.text("<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <link rel="stylesheet" href="assets/css/styles.css">
-        <link href="https://fonts.cdnfonts.com/css/bruno-ace" rel="stylesheet">
-    <style>
-      body {
-        background-color: #810065;
-      }
-        .center-screen {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            text-align: center;
-            font-family: Bruno Ace;
-        }
-    </style>
-</head>
-<body>
-    <div class="center-screen">
-        <h1>Success, Stay tuned.</h1>
-    </div>
-</body>
-</html>
-");
+  return res.sendFile('/success.html');
 }
